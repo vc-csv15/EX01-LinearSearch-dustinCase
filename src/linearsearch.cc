794af1396ecb5daf520a8e4d0e100b7b27019039
Dustin <dustin8800@hotmail.com>
2019-09-11 14:41:41 -0700

    Committing!

--- src/linearsearch.cc
+++ src/linearsearch.cc
@@ -7,14 +7,14 @@
 //#include "linearsearch.h"
 
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
-    template <class T>
-    int64_t find(const T& value, T array[], size_t size) {
-        if (array == NULL) return -1;
-        for (int i = 0; i < size; i++) {
-            if (value == array[i]) return i;
-        }
-
-        return(-1);
-    }
+//    template <class T>
+//    int64_t find(const T& value, T array[], size_t size) {
+//        if (array == NULL) return -1;
+//        for (int i = 0; i < size; i++) {
+//            if (value == array[i]) return i;
+//        }
+//
+//        return(-1);
+//    }
 
 }}}}
